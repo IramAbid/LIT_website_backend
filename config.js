6b9a2942e9d135ee78065699ca4d3c29df3510eb
ayaankhan98 <ayaankhan98@gmail.com>
2023-01-24 11:53:49 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+let config = {
+    PORT: 3000,
+}
+export default config;
---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import config from "./config.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.get("/api/test", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(config.PORT, () => {
+    console.log(`server running on ${config.PORT}`);
+});
