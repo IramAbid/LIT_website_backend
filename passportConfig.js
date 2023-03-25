dbc82f677c079928c89c3158885fadfceb24d1b9
IramAbid <irama4633@gmail.com>
2023-03-26 01:29:15 +0530

    Now able to login, checking type of user on login is left

--- passportConfig.js
+++ passportConfig.js
@@ -3,16 +3,16 @@ const { pool } = require("./dbConfig");
 const bcrypt = require("bcrypt");
 
 function initialize(passport) {
-  console.log("Initialized");
+  console.log("Initialize");
 
+  
   const authenticateUser = (email, password, done) => {
-    console.log(email, password);
     pool.query(
-      `SELECT * FROM users WHERE email = $1`,
+      `SELECT * FROM userauth WHERE email = $1`,
       [email],
       (err, results) => {
         if (err) {
-          throw err;
+          return done(err);
         }
         console.log(results.rows);
 
@@ -21,20 +21,17 @@ function initialize(passport) {
 
           bcrypt.compare(password, user.password, (err, isMatch) => {
             if (err) {
-              console.log(err);
+              return done(err);
             }
+
             if (isMatch) {
               return done(null, user);
             } else {
-              //password is incorrect
-              return done(null, false, { message: "Password is incorrect" });
+              return done(null, false, { message: "Password is not correct" });
             }
           });
         } else {
-          // No user
-          return done(null, false, {
-            message: "No user with that email address"
-          });
+          return done(null, false, { message: "Email entered is not registered" });
         }
       }
     );
@@ -42,26 +39,24 @@ function initialize(passport) {
 
   passport.use(
     new LocalStrategy(
-      { usernameField: "email", passwordField: "password" },
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
       authenticateUser
     )
   );
-  // Stores user details inside session. serializeUser determines which data of the user
-  // object should be stored in the session. The result of the serializeUser method is attached
-  // to the session as req.session.passport.user = {}. Here for instance, it would be (as we provide
-  //   the user id as the key) req.session.passport.user = {id: 'xyz'}
-  passport.serializeUser((user, done) => done(null, user.id));
 
-  // In deserializeUser that key is matched with the in memory array / database or any data resource.
-  // The fetched object is attached to the request object as req.user
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
   passport.deserializeUser((id, done) => {
-    pool.query(`SELECT * FROM users WHERE id = $1`, [id], (err, results) => {
+    pool.query(`SELECT * FROM userauth WHERE id = $1`, [id], (err, results) => {
       if (err) {
-        return done(err);
+        throw err;
       }
-      console.log(`ID is ${results.rows[0].id}`);
-      return done(null, results.rows[0]);
+      done(null, results.rows[0]);
     });
   });
 }
